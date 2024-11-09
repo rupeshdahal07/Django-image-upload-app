e3673cf690a220f72778b38d158efc424b944369
rupeshdahal07 <rupyesdahal12@gmail.com>
2024-11-09 21:25:02 +0545

    update

--- imageuploder/myapp/models.py
+++ imageuploder/myapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    title = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='images/')
+    description = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+    
--- imageuploder/myapp/views.py
+++ imageuploder/myapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Image
+from .form import Imageform
 
 # Create your views here.
+def home(request):
+    if request.method == 'POST':
+        form = Imageform(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+    form = Imageform()
+    return render(request, 'myapp/home.html', {'form': form, 'images': Image.objects.all()})
